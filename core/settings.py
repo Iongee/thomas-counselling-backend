53d1c1e00e8d5742aa79a379672f3f3a37f7b5a2
funny0619 <funny0619@naver.com>
2025-07-29 16:35:30 +0800

    ngrok admin

--- core/settings.py
+++ core/settings.py
@@ -85,8 +85,16 @@ CHANNEL_LAYERS = {
 # }
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173","https://lets-talk-counselling.netlify.app"]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:5173',"https://lets-talk-counselling.netlify.app"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://lets-talk-counselling.netlify.app",
+    "https://*.ngrok-free.app"
+]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173',
+    "https://lets-talk-counselling.netlify.app",
+    "https://*.ngrok-free.app"
+]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
